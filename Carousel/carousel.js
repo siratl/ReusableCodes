5811271dc9f3831a08699f9875c40b294bbe0e27
siratl <elishubax@gmail.com>
2018-12-18 18:16:09 -0800

    added an event listener to stop slide on mouse enter and coninue slide on mouse leave

--- Carousel/carousel.js
+++ Carousel/carousel.js
@@ -1,16 +1,31 @@
 class Carousel {
+    // create a constructor that accepts the ".carousel" container as a parameter
     constructor(wrapperElement) {
+        // Reference and assign the parameter/container to a constructor variable
         this.wrapperElement = document.querySelector(wrapperElement);
+        // Reference every child/element within the container and assign them as children
         this.children = this.wrapperElement.children;
-        this.wrapperElementWidth = this.wrapperElement.offsetWidth;
-        // this.wrapperElement.style.width = (this.wrapperElementWidth / this.children.length) + "px"
+        //** this.wrapperElementWidth = this.wrapperElement.offsetWidth; **/
 
+        // assign and index counter
         this.currentIndex = 0;
+        // Create an array from the aray-like children element to inherit methods available to Arrays (.forEach)
+        // For each child element, set the display to none
         Array.from(this.children).forEach(image => image.style.display = "none");
+        // Make the child element at the current index visible
         this.children[this.currentIndex].style.display = "block";
-        setInterval(() => this.slide(), 3000);
+        // Created a variable to store time
+        let timeLoop = 1000;
+        // Assigned the setInterval method to a variable "timer" that runs the method "slide"
+        var timer = setInterval(() => this.slide(), timeLoop);
+        // Added an event listener on the carousel container to pause the "slide" on mouse enter
+        this.wrapperElement.addEventListener("mouseenter", () => clearInterval(timer))
+
+        // Added an event listener on the carousel container to continue the "slide" method on mouse leave
+        this.wrapperElement.addEventListener("mouseleave", () => timer = setInterval(() => this.slide(), timeLoop))
     }
 
+    // Methods
     slide() {
         this.children[this.currentIndex].style.display = "none";
         this.currentIndex++;
@@ -23,5 +38,5 @@ class Carousel {
 
 window.addEventListener("load", function (){
     new Carousel(".carousel");
-    
 })
+
